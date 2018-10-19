5b082aae1a23cbadb8779192046616b52fdbd209
Kanwei Li <kanwei@gmail.com>
2018-10-19 12:48:55 -0400

    Initial

---
+++ test/ring_undertow_simple/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-undertow-simple.core-test
+  (:require [clojure.test :refer :all]
+            [ring-undertow-simple.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
